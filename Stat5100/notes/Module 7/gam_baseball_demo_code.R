7bdfef2364c5a5fe47372c68e507e9490f7c9e5b
Ethan Ancell <ethanancell@gmail.com>
2021-04-11 19:18:40 -0600

    Added completed 7.1 Notes

---
+++ Stat5100/notes/Module 7/gam_baseball_demo_code.R
@@ -0,0 +1,32 @@
+crAtBat_range <- seq(min(baseball$CrAtBat), max(baseball$CrAtBat), length = 500)
+nBB_range <- seq(min(baseball$nBB), max(baseball$nBB), length = 500)
+baseball_gam_short <- gam::gam(logSalary ~ s(CrAtBat) + s(nBB), data = baseball)
+gam_pred <- function(x, y) {
+  predict(baseball_gam_short, data.frame(CrAtBat = x,
+                                         nBB = y))
+}
+
+z <- outer(crAtBat_range, nBB_range, gam_pred)
+
+library(plotly)
+fig = plot_ly(
+  type = "surface",
+  x = ~crAtBat_range,
+  y = ~nBB_range,
+  z = ~z
+)
+fig <- plotly::add_markers(
+  fig,
+  x = ~baseball$CrAtBat,
+  y = ~baseball$nBB,
+  z = ~baseball$logSalary,
+  size = I(30),
+  color = I("#FF0000")
+)
+fig <- plotly::layout(
+  fig,
+  scene = list(xaxis = list(title = "Career Times at Bat"),
+               yaxis = list(title = "Walks in 1986"),
+               zaxis = list(title = "Log Salary"))
+)
+fig
